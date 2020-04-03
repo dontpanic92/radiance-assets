76d4e88b54d955fa4986fc7620b362d5d8da9663
Li Shengqiu <lishengqiu.hit@gmail.com>
2020-04-03 19:43:15 +0800

    Remove SourceHanSans

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,2 @@
 pub static TEXTURE_MISSING_TEXTURE_FILE: &'static [u8] = include_bytes!("embed/textures/texture_missing.png");
-
-pub static FONT_SOURCE_HAN_SANS: &'static [u8] = include_bytes!("embed/fonts/SourceHanSans.ttc");
 pub static FONT_SOURCE_HAN_SERIF: &'static [u8] = include_bytes!("embed/fonts/SourceHanSerif-Regular.ttf");
