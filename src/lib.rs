dc021db37d961504d0c71d5302cf938c41780268
Shengqiu Li <lishengqiu.hit@gmail.com>
2023-08-13 22:27:18 +0800

    add white texture

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
-pub static TEXTURE_MISSING_TEXTURE_FILE: &'static [u8] = include_bytes!("embed/textures/texture_missing.png");
-pub static FONT_SOURCE_HAN_SERIF: &'static [u8] = include_bytes!("embed/fonts/SourceHanSerif-Regular.ttf");
+pub static TEXTURE_MISSING_TEXTURE_FILE: &'static [u8] =
+    include_bytes!("embed/textures/texture_missing.png");
+pub static FONT_SOURCE_HAN_SERIF: &'static [u8] =
+    include_bytes!("embed/fonts/SourceHanSerif-Regular.ttf");
